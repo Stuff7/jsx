b1afdcbed304a370ab14e640698a3fa7ff1facac
Stuff7 <cuentafrst@gmail.com>
2024-10-11 22:12:43 -0600

    script

--- build_sample.sh
+++ build_sample.sh
@@ -1,2 +1,2 @@
-cargo run --bin jsx_template logs/test_dir/ -import '~/runtime' > build/index.js
-./node_modules/esbuild/bin/esbuild build/index.js --bundle --sourcemap --jsx=preserve --minify --outdir=sample
+cargo run --bin jsx_template logs/test_dir/ -import '~/runtime' > $1
+./node_modules/esbuild/bin/esbuild $1 --bundle --sourcemap --minify --outdir=sample
